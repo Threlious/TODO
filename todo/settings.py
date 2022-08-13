067d003946e24cfcd019f28d57f33c5c3f30da86
Garret <ma_melnik97@mail.ru>
2022-08-13 12:58:50 +0300

    lesson 2

--- todo/settings.py
+++ todo/settings.py
@@ -32,6 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000'
+]
+
 
 # Application definition
 
@@ -46,11 +51,13 @@ INSTALLED_APPS = [
     'authors',
 
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
