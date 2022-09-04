986a29ccbe7225109f37ffd6372fbac6632d04bd
Garret <ma_melnik97@mail.ru>
2022-09-04 11:38:31 +0300

    lesson 7

--- todoapp/views.py
+++ todoapp/views.py
@@ -21,7 +21,7 @@ class TODOModelViewSet(ModelViewSet):
     serializer_class = TODOModelSerializer
     pagination_class = TODOLimitOffsetPagination
     filterset_fields = ['project', ]
-    # permission_classes = [SuperUserOnly, ]
+    permission_classes = [SuperUserOnly, ]
 
     def destroy(self, request, *args, **kwargs):
         todo = self.get_object()
